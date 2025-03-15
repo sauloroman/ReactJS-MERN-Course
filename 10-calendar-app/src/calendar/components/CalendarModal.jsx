f13a2d04098eb43655131380126df3131e4d0f5d
Saulo Roman Santillan Nava <romansantillan1998@outlook.com>
2025-03-14 23:35:31 -0600

    React Redux settings

--- 10-calendar-app/src/calendar/components/CalendarModal.jsx
+++ 10-calendar-app/src/calendar/components/CalendarModal.jsx
@@ -1,8 +1,14 @@
+import { useMemo, useState } from 'react';
 import { addHours, differenceInSeconds } from 'date-fns';
-import { useState } from 'react';
+
+import Swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
+
 import Modal from 'react-modal';
+
 import DatePicker, {registerLocale} from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+
 import es from 'date-fns/locale/es';
 
 registerLocale('es', es)
@@ -22,6 +28,7 @@ Modal.setAppElement('#root');
 
 export const CalendarModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(true);
+  const [formSubmitted, setFormSubmitted] = useState(false)
 
   const [formValues, setFormValues] = useState({
     title: 'Roman',
@@ -29,6 +36,11 @@ export const CalendarModal = () => {
     start: new Date(),
     end: addHours( new Date(), 2 )
   })
+
+  const titleClass = useMemo(() => {
+    if ( !formSubmitted ) return ''
+    return ( formValues.title.length <= 0 ) && 'is-invalid'
+  },[ formValues.title, formSubmitted ])
 
   const onInputChanged = ({ target }) => {
     setFormValues({
@@ -50,11 +62,12 @@ export const CalendarModal = () => {
 
   const onSubmit = (e) => {
     e.preventDefault()
+    setFormSubmitted(true)
 
     const difference = differenceInSeconds( formValues.end, formValues.start )
     
     if ( isNaN(difference) || difference <= 0 ) {
-      console.log('Error en fechas')
+      Swal.fire('Fechas incorrectas', 'Revisar las fechas ingresadas', 'error')
       return
     }
 
@@ -112,7 +125,7 @@ export const CalendarModal = () => {
           <label>Titulo y notas</label>
           <input
             type="text"
-            className="form-control"
+            className={`form-control ${titleClass}`}
             placeholder="Título del evento"
             name="title"
             autoComplete="off"
---
+++ 10-calendar-app/src/store/index.js
@@ -0,0 +1,2 @@
+export * from './ui/ui.slice'
+export * from './store'
